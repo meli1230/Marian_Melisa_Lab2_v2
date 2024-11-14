45bbbab735857336550463231e484b76d3309763
meli1230 <melisa_marian@yahoo.fr>
2024-11-14 10:56:58 +0200

    ex_2

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages(options =>
-{
-    options.Conventions.AuthorizeFolder("/Books");
-    options.Conventions.AllowAnonymousToPage("/Books/Index");
-    options.Conventions.AllowAnonymousToPage("/Books/Details");
-});
+builder.Services.AddRazorPages(
+    options =>
+    {
+        options.Conventions.AuthorizeFolder("/Books");
+        options.Conventions.AllowAnonymousToPage("/Books/Index");
+        options.Conventions.AllowAnonymousToPage("/Books/Details");
+    });
 builder.Services.AddDbContext<Marian_Melisa_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Marian_Melisa_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Marian_Melisa_Lab2Context' not found.")));
 
@@ -21,6 +22,7 @@ builder.Services.AddDbContext<LibraryIdentityContext>(options => options.UseSqlS
 
 builder.Services.AddDefaultIdentity<IdentityUser>(
     options => options.SignIn.RequireConfirmedAccount = true)
+        .AddRoles<IdentityRole>()
         .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
